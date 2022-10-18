899dfe865da98ddb1f7d480ec8478e1940a77b7e
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-10-18 23:25:58 +0000

    added hearts, onClick event, constructor and handleHeart method inside HornedBeast component

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -4,16 +4,30 @@ import React from "react";
 
 // class component
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      heart: 0
+    }
+  }
+
+  handleHeart = () => {
+    this.setState = {
+      heart: this.state.heart + 1
+    }
+  }
+
   render() {
     return (
       <>
       <h2>{this.props.title}</h2>
+      <p onClick={this.handleHeart}>♥️ favorites</p>
       <img 
         src={this.props.src} 
         alt={this.props.alt}
         title={this.props.title} 
       />
-      <p>{this.props.description}</p>
+      <p id="description">{this.props.description}</p>
       </>
     )
   }
