42a08b9dae16a85a7b14eae13da147b0c99dac9e
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-10-18 01:02:32 +0000

    created header and main component

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+// import
+import React from "react";
+
+
+class Header extends React.Component {
+  render() {
+    return(
+      <header>
+        <h1>Hello World!</h1>
+      </header>
+    )
+  }
+}
+
+export default Header 
---
+++ src/Main.js
@@ -0,0 +1,16 @@
+// import
+import React from "react";
+
+
+// class
+class Main extends React.Component {
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+// export
+export default Main;
