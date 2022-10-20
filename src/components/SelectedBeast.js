3a26971132da550a635f679e75ce6879a2326cbb
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-10-20 02:17:05 +0000

    added row

--- src/components/SelectedBeast.js
+++ src/components/SelectedBeast.js
@@ -8,14 +8,16 @@ class SelectedBeast extends React.Component {
   render() {
     return (
 
+      <Row className="row" xs={1} sm={2} md={3} lg={4}>
+
       <Modal className="modal"
         show = {this.props.show}
         onHide = {this.props.handleCloseModal}
       >
         
         <Modal.Header className="closedBtnModal" closeButton>
-          <Modal.Body>
-
+          <Modal.Body className="row">
+            {this.props.title}
             <img className="selectedImg" src={this.props.selectedImage} alt={this.props.title} title={this.props.title}></img>
 
             <Row className="row" xs={1} sm={2} md={3} lg={4}>
@@ -25,7 +27,7 @@ class SelectedBeast extends React.Component {
           </Modal.Body>
         </Modal.Header>
 
-      </Modal >
+      </Modal></Row>
 
     )
   } 
